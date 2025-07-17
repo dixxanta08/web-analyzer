98e1a5881707261c5f57dafd13f66f86955273f4
dixxanta08 <dixantashrestha08@gmail.com>
2025-07-17 06:26:43 +0545

    Vercel pushed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/web-analyzer/",
+  base: "./",
   optimizeDeps: {
     include: ["react", "react-dom", "react-icons/ri"],
   },
